8b60f4a11f576b5c50db79712267b32734babb96
vitthal4545 <vitthalgs1434@gmail.com>
2025-02-06 23:34:31 +0530

    fixed the index route issue

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV != "Production") {
-    require("dotenv").config();
+if (process.env.NODE_ENV != "Production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -24,49 +24,47 @@ const dbUrl = process.env.ATLASDB_URL;
 mongoose.set("debug", true);
 
 async function main() {
-    await mongoose.connect(dbUrl);
+  await mongoose.connect(dbUrl);
 }
 
-
-main().then((res) => {
+main()
+  .then((res) => {
     console.log("DB connection successful");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
-
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600, 
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
 store.on("error", () => {
-    console.log("ERROR IN MONGO SESSION STORE", err);
+  console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
-    resave: false, 
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
-}
-
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -78,32 +76,43 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currentUser = req.user;
-    next();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
+  next();
 });
 
+//changes
 
+const Listing = require("./models/listing.js"); // Import Listing model
+
+app.get("/", async (req, res) => {
+  try {
+    console.log(req.user);
+    const listings = await Listing.find(); // Fetch listings from DB
+    res.render("listings/index", { listings }); // Render same page as /listings
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//changes end
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-
-
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not found"));
+  next(new ExpressError(404, "Page not found"));
 });
-
 
 app.use((err, req, res, next) => {
-    let{ statusCode=500, message="Something went wrong!" } = err;
-    res.status(statusCode).render("listings/error.ejs", { message });
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("listings/error.ejs", { message });
 });
 
-app.listen(8080, '0.0.0.0', () => {
-    console.log("the server is listening. you are online now......💀⚡"); 
+app.listen(8080, () => {
+  console.log("the server is listening. you are online now......💀⚡");
 });
